dd165f6851d0394a3a9213249cedc329a7ab8c47
Luis Alvarez <laar96@unitec.edu>
2018-06-03 00:37:50 -0600

    prueba de Body agregada

---
+++ src/__tests__/Body.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import Body from './../Body';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Body />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing', () => {
+  shallow(<Body />);
+});
+
+test('renders correctly', () => {
+  const tree = renderer.create(<Body />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
