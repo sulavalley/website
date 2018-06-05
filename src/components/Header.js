1b92c7b0c50a201c1c0e0aa8b1dd5bb159447d33
Luis Alvarez <laar96@unitec.edu>
2018-06-04 23:46:15 -0600

    dependencies deleted

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './../css/Header.css';
+import logo from './../assets/images/logo.png';
 
 const Header = () => {
   return (
     <div className="ui header-size menu">
-      <img src={require('./../assets/images/logo.png')} alt="logo" />
+      <img src={logo} alt="logo" />
       <div className="right menu">
         <a className="item" href="/institucionalidad">
           Institucionalidad
