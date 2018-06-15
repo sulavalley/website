5535ffdce680a23d569ea5b837e5d5f932009961
RDRR7 <rene.d.rendon@unitec.edu>
2018-06-15 00:48:19 -0600

    Added video player for multimedia page
    Co-authored-by: Lictro <laar96@unitec.edu>

--- src/components/pages/Multimedia.js
+++ src/components/pages/Multimedia.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Embed } from 'semantic-ui-react';
+import '../../css/pages/Multimedia.css';
 
-const Multimedia = () => {
-  return (
-    <div>
-      <h1>Estas en Multimedia</h1>
+const Multimedia = () => (
+  <center>
+    <div className="video-player">
+      <Embed
+        url="https://www.youtube.com/embed/videoseries?list=UUffHDdNhOCZFvt-f_D4kxFA"
+        placeholder="https://trello-attachments.s3.amazonaws.com/5aff552929693747ec0b088d/5aff5c23f3cddefc5870f975/34b908e23ae35dd9a286309203978edd/logo_MRT-VS-HD.png"
+        iframe={{
+          allowFullScreen: true
+        }}
+      />
     </div>
-  );
-};
+  </center>
+);
 
 export default Multimedia;
