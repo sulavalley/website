dec302368a4183005c5944fad87adfcaab4899b1
RDRR7 <rene.d.rendon@unitec.edu>
2018-06-22 00:45:36 -0600

    Added image gallery for multimedia page
    Co-authored-by: Lictro <laar96@unitec.edu>

--- src/components/pages/Multimedia.js
+++ src/components/pages/Multimedia.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Embed } from 'semantic-ui-react';
-import '../../css/pages/Multimedia.css';
+import { Tab } from 'semantic-ui-react';
+import VideoPlayer from '../VideoPlayer';
+import ImageGallery from '../ImageGallery';
+
+const panes = [
+  {
+    menuItem: 'Imágenes',
+    render: () => (
+      <Tab.Pane>
+        <ImageGallery />
+      </Tab.Pane>
+    )
+  },
+  {
+    menuItem: 'Vídeos',
+    render: () => (
+      <Tab.Pane>
+        <VideoPlayer
+          url={
+            'https://www.youtube.com/embed/videoseries?list=UUffHDdNhOCZFvt-f_D4kxFA'
+          }
+        />
+      </Tab.Pane>
+    )
+  }
+];
 
 const Multimedia = () => (
   <center>
-    <div className="video-player">
-      <Embed
-        url="https://www.youtube.com/embed/videoseries?list=UUffHDdNhOCZFvt-f_D4kxFA"
-        placeholder="https://trello-attachments.s3.amazonaws.com/5aff552929693747ec0b088d/5aff5c23f3cddefc5870f975/34b908e23ae35dd9a286309203978edd/logo_MRT-VS-HD.png"
-        iframe={{
-          allowFullScreen: true
-        }}
-      />
-    </div>
+    <Tab panes={panes} />
   </center>
 );
 
