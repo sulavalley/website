7066be552dd4a96f332a6cd555e1e8b72ca953bc
Victor Vasquez <victor.vasquez96@unitec.edu>
2018-06-25 02:46:02 -0600

    Created concept view for institutionality

--- src/components/pages/Institutionality.js
+++ src/components/pages/Institutionality.js
@@ -2,8 +2,70 @@ import React from 'react';
 
 const Institutionality = () => {
   return (
-    <div>
-      <h1>Estas en Institucionalidad</h1>
+    <div className="ui items">
+      <div className="item" align="center">
+        <div className="content">
+          <h1>Mision</h1>
+          <div className="description" style={{ maxWidth: '50%' }}>
+            <p>
+              {' '}
+              Nulla aliquet elit non nulla pellentesque placerat sed malesuada
+              metus. Fusce ac odio et augue convallis consequat. Fusce
+              ullamcorper nulla vitae dui molestie tristique cursus suscipit
+              massa. Suspendisse pulvinar quam sed orci bibendum vestibulum.
+              Curabitur libero neque, interdum ornare mi et, auctor hendrerit
+              lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
+              In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
+              felis, efficitur semper turpis imperdiet quis.{' '}
+            </p>
+          </div>
+        </div>
+        <div className="image">
+          <img src="/images/wireframe/image.png" />
+        </div>
+      </div>
+      <div className="item">
+        <div className="image">
+          <img src="/images/wireframe/image.png" />
+        </div>
+        <div className="content" align="center">
+          <h1>Vision</h1>
+          <div className="description" style={{ maxWidth: '50%' }}>
+            <p>
+              {' '}
+              Nulla aliquet elit non nulla pellentesque placerat sed malesuada
+              metus. Fusce ac odio et augue convallis consequat. Fusce
+              ullamcorper nulla vitae dui molestie tristique cursus suscipit
+              massa. Suspendisse pulvinar quam sed orci bibendum vestibulum.
+              Curabitur libero neque, interdum ornare mi et, auctor hendrerit
+              lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
+              In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
+              felis, efficitur semper turpis imperdiet quis.{' '}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="item">
+        <div className="content" align="center">
+          <h1>Objetivos</h1>
+          <div className="description" style={{ maxWidth: '50%' }}>
+            <p>
+              {' '}
+              Nulla aliquet elit non nulla pellentesque placerat sed malesuada
+              metus. Fusce ac odio et augue convallis consequat. Fusce
+              ullamcorper nulla vitae dui molestie tristique cursus suscipit
+              massa. Suspendisse pulvinar quam sed orci bibendum vestibulum.
+              Curabitur libero neque, interdum ornare mi et, auctor hendrerit
+              lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
+              In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
+              felis, efficitur semper turpis imperdiet quis.{' '}
+            </p>
+          </div>
+        </div>
+        <div className="image">
+          <img src="/images/wireframe/image.png" />
+        </div>
+      </div>
     </div>
   );
 };
