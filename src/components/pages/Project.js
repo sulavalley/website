58bbe2144060190bb778afce8c53623e960456b8
RDRR7 <rene.d.rendon@unitec.edu>
2018-06-27 00:11:32 -0600

    Added grid for projects

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,19 +4,24 @@ import { Card, Image } from 'semantic-ui-react';
 
 class Project extends React.Component {
   render() {
-    const { name , url, description } = this.props;
- 
+    const { name, url, description } = this.props;
+
     return (
-      <Card>
+      <Card
+        style={{
+          minHeight: '100%'
+        }}
+      >
         <Image
+          style={{
+            marginLeft: '0%'
+          }}
           src={url}
           size="medium"
         />
         <Card.Content>
           <Card.Header>{name}</Card.Header>
-          <Card.Description>
-            {description}
-          </Card.Description>
+          <Card.Description>{description}</Card.Description>
         </Card.Content>
       </Card>
     );
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import base from '../../base';
 import Project from './Project';
+import { Grid } from 'semantic-ui-react';
 
 class Projects extends React.Component {
-
   state = {
     projects: []
-  }
+  };
 
-componentDidMount (){
-  base.fetch('projects', {
+  componentDidMount() {
+    base.fetch('projects', {
       context: this,
       asArray: true,
       then(data) {
-        this.setState({projects: data})
+        this.setState({ projects: data });
       }
     });
-}
+  }
   render() {
-
     return (
       <div>
-        {this.state.projects.map((project,i) => (<Project name= {project.name} 
-          url= {project.url} description= {project.description} key={i}/>))}
+        <Grid columns={4}>
+          {this.state.projects.map((project, i) => (
+            <Grid.Column>
+              <Project
+                name={project.name}
+                url={project.url}
+                description={project.description}
+                key={i}
+              />
+            </Grid.Column>
+          ))}
+        </Grid>
       </div>
-    )
+    );
   }
 }
 
