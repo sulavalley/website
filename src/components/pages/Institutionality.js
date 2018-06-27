ca21b34595e86bec7d14e75c36acc65c8f2ed9e9
RDRR7 <rene.d.rendon@unitec.edu>
2018-06-27 01:26:35 -0600

    Added images for institutionality page and updated tests

--- src/components/pages/Institutionality.js
+++ src/components/pages/Institutionality.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react';
+import image3 from './../../assets/images/image3.jpg';
+import image5 from './../../assets/images/image5.jpg';
+import image22 from './../../assets/images/image22.jpg';
+import image28 from './../../assets/images/image28.jpg';
 
 const Institutionality = () => {
   return (
     <div className="ui items">
       <div className="item" align="center">
         <div className="content">
-          <h1>Mision</h1>
+          <h1>Misión</h1>
           <div className="description" style={{ maxWidth: '50%' }}>
             <p>
-              {' '}
               Nulla aliquet elit non nulla pellentesque placerat sed malesuada
               metus. Fusce ac odio et augue convallis consequat. Fusce
               ullamcorper nulla vitae dui molestie tristique cursus suscipit
@@ -16,23 +20,22 @@ const Institutionality = () => {
               Curabitur libero neque, interdum ornare mi et, auctor hendrerit
               lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
               In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
-              felis, efficitur semper turpis imperdiet quis.{' '}
+              felis, efficitur semper turpis imperdiet quis.
             </p>
           </div>
         </div>
         <div className="image">
-          <img src="/images/wireframe/image.png" />
+          <Image src={image3} />
         </div>
       </div>
       <div className="item">
         <div className="image">
-          <img src="/images/wireframe/image.png" />
+          <Image src={image5} />
         </div>
         <div className="content" align="center">
-          <h1>Vision</h1>
+          <h1>Visión</h1>
           <div className="description" style={{ maxWidth: '50%' }}>
             <p>
-              {' '}
               Nulla aliquet elit non nulla pellentesque placerat sed malesuada
               metus. Fusce ac odio et augue convallis consequat. Fusce
               ullamcorper nulla vitae dui molestie tristique cursus suscipit
@@ -40,7 +43,7 @@ const Institutionality = () => {
               Curabitur libero neque, interdum ornare mi et, auctor hendrerit
               lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
               In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
-              felis, efficitur semper turpis imperdiet quis.{' '}
+              felis, efficitur semper turpis imperdiet quis.
             </p>
           </div>
         </div>
@@ -50,7 +53,6 @@ const Institutionality = () => {
           <h1>Objetivos</h1>
           <div className="description" style={{ maxWidth: '50%' }}>
             <p>
-              {' '}
               Nulla aliquet elit non nulla pellentesque placerat sed malesuada
               metus. Fusce ac odio et augue convallis consequat. Fusce
               ullamcorper nulla vitae dui molestie tristique cursus suscipit
@@ -58,12 +60,32 @@ const Institutionality = () => {
               Curabitur libero neque, interdum ornare mi et, auctor hendrerit
               lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
               In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
-              felis, efficitur semper turpis imperdiet quis.{' '}
+              felis, efficitur semper turpis imperdiet quis.
             </p>
           </div>
         </div>
         <div className="image">
-          <img src="/images/wireframe/image.png" />
+          <Image src={image22} />
+        </div>
+      </div>
+      <div className="item">
+        <div className="image">
+          <Image src={image28} />
+        </div>
+        <div className="content" align="center">
+          <h1>Alcance</h1>
+          <div className="description" style={{ maxWidth: '50%' }}>
+            <p>
+              Nulla aliquet elit non nulla pellentesque placerat sed malesuada
+              metus. Fusce ac odio et augue convallis consequat. Fusce
+              ullamcorper nulla vitae dui molestie tristique cursus suscipit
+              massa. Suspendisse pulvinar quam sed orci bibendum vestibulum.
+              Curabitur libero neque, interdum ornare mi et, auctor hendrerit
+              lacus. Aliquam erat volutpat. In sollicitudin convallis hendrerit.
+              In vel sodales diam, vitae sagittis massa. Nunc vulputate faucibus
+              felis, efficitur semper turpis imperdiet quis.
+            </p>
+          </div>
         </div>
       </div>
     </div>
