17328a172dc78772c36fe273ea154e2931c203c8
astrothing <andres.rodriguezhn@gmail.com>
2018-07-02 23:25:34 -0600

    All tests passing now
    Co-authored-by: RDRR7 <rene.d.rendon@unitec.edu>

--- src/__tests__/VideoPlayer.test.js
+++ src/__tests__/VideoPlayer.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
-import VideoPlayer from '../components/VideoPlayer';
+import VideoPlayer from '../components/pages/Multimedia/VideoPlayer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
