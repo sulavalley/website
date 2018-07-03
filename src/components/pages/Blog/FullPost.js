d5e1d244f580a75a870ec7bb59fbf27eb730c7cf
astrothing <andres.rodriguezhn@gmail.com>
2018-07-02 22:38:04 -0600

    Added images to blog posts
    Co-authored-by: RDRR7 <rene.d.rendon@unitec.edu>

--- src/components/pages/Blog/FullPost.js
+++ src/components/pages/Blog/FullPost.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import Disqus from 'disqus-react';
 import base from '../../../base';
 import unixToDate from './unixToDate';
-import { Item } from 'semantic-ui-react';
+import { Item, Image } from 'semantic-ui-react';
+import logo from '../../../assets/images/logo.png';
 
 const shortname = 'sulavalley';
 
 class FullPost extends Component {
-  state = { title: '', author: '', timestamp: null, content: '' };
+  state = { title: '', author: '', timestamp: null, content: '', imageURL: '' };
 
   componentDidMount() {
     if (this.props.location.state === undefined) {
@@ -23,8 +24,12 @@ class FullPost extends Component {
     }
   }
 
+  handleError = evt => {
+    evt.target.src = logo;
+  };
+
   render() {
-    const { title, author, timestamp, content } = this.state;
+    const { title, author, timestamp, content, imageURL } = this.state;
     const { id } = this.props.match.params;
     const disqusConfig = {
       url: window.location.href,
@@ -33,22 +38,30 @@ class FullPost extends Component {
     };
 
     return (
-      <Item.Group>
-        <Item>
-          <Item.Content>
-            <Item.Header>{title}</Item.Header>
-            <Item.Extra>
-              <span>{author}</span>
-              <span>{unixToDate(timestamp)}</span>
-            </Item.Extra>
-            <Item.Description>{content}</Item.Description>
-            <Disqus.DiscussionEmbed
-              shortname={shortname}
-              config={disqusConfig}
-            />
-          </Item.Content>
-        </Item>
-      </Item.Group>
+      <div>
+        <Image
+          style={{ margin: '0 auto' }}
+          src={imageURL}
+          onError={this.handleError}
+          size="medium"
+        />
+        <Item.Group>
+          <Item>
+            <Item.Content>
+              <Item.Header>{title}</Item.Header>
+              <Item.Extra>
+                <span>{author}</span>
+                <span>{unixToDate(timestamp)}</span>
+              </Item.Extra>
+              <Item.Description>{content}</Item.Description>
+              <Disqus.DiscussionEmbed
+                shortname={shortname}
+                config={disqusConfig}
+              />
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </div>
     );
   }
 }
