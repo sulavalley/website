badee9b3cef9f589b397cc55157814f2f1c0058e
astrothing <andres.rodriguezhn@gmail.com>
2018-07-02 21:28:58 -0600

    Reworked Instituionality
    Co-authored-by: RDRR7 <rene.d.rendon@unitec.edu>

--- src/components/pages/Institutionality/index.js
+++ src/components/pages/Institutionality/index.js
@@ -1,56 +1,101 @@
 import React from 'react';
-import { Item, Header } from 'semantic-ui-react';
+import { Grid, Header, Image } from 'semantic-ui-react';
 import image3 from '../../../assets/images/image3.jpg';
 import image5 from '../../../assets/images/image5.jpg';
 import image22 from '../../../assets/images/image22.jpg';
 import image28 from '../../../assets/images/image28.jpg';
+import '../../../css/pages/Institutionality.css';
 
 const loremIpsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const Institutionality = () => {
   return (
-    <Item.Group divided className="clearing">
-      <Item>
-        <Item.Image size="medium" src={image3} />
-        <Item.Content>
-          <Header size="huge">Misión</Header>
-          <Item.Description>
+    <Grid container>
+      <Grid.Row>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={4}>
+            <div className="image-container">
+              <Image
+                className="grid-image"
+                rounded
+                size="medium"
+                src={image3}
+              />
+            </div>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <Header textAlign="center" size="huge">
+              Misión
+            </Header>
             <p>{loremIpsum}</p>
-          </Item.Description>
-        </Item.Content>
-      </Item>
+          </Grid.Column>
+        </Grid>
+      </Grid.Row>
 
-      <Item>
-        <Item.Image size="medium" src={image5} />
-        <Item.Content>
-          <Header size="huge">Visión</Header>
-          <Item.Description>
+      <Grid.Row>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={12}>
+            <Header textAlign="center" size="huge">
+              Visión
+            </Header>
             <p>{loremIpsum}</p>
-          </Item.Description>
-        </Item.Content>
-      </Item>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <div className="image-container">
+              <Image
+                className="grid-image"
+                rounded
+                size="medium"
+                src={image5}
+              />
+            </div>
+          </Grid.Column>
+        </Grid>
+      </Grid.Row>
 
-      <Item>
-        <Item.Image size="medium" src={image22} />
-        <Item.Content>
-          <Header size="huge">Objetivos</Header>
-          <Item.Description>
+      <Grid.Row>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={4}>
+            <div className="image-container">
+              <Image
+                className="grid-image"
+                rounded
+                size="medium"
+                src={image22}
+              />
+            </div>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <Header textAlign="center" size="huge">
+              Objetivos
+            </Header>
             <p>{loremIpsum}</p>
-          </Item.Description>
-        </Item.Content>
-      </Item>
+          </Grid.Column>
+        </Grid>
+      </Grid.Row>
 
-      <Item>
-        <Item.Image size="medium" src={image28} />
-        <Item.Content>
-          <Header size="huge">Alcance</Header>
-          <Item.Description>
+      <Grid.Row>
+        <Grid verticalAlign="middle">
+          <Grid.Column width={12}>
+            <Header textAlign="center" size="huge">
+              Alcance
+            </Header>
             <p>{loremIpsum}</p>
-          </Item.Description>
-        </Item.Content>
-      </Item>
-    </Item.Group>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <div className="image-container">
+              <Image
+                className="grid-image"
+                rounded
+                size="medium"
+                src={image28}
+              />
+            </div>
+          </Grid.Column>
+        </Grid>
+      </Grid.Row>
+    </Grid>
   );
 };
 
